f6b2d257e8d65b6c75c15b19152ce453250f7a16
Aleksander Lisiecki <sebaczechu123@gmail.com>
2019-12-11 12:15:14 +0100

    Change handler message

--- src/handlers/hello_world.erl
+++ src/handlers/hello_world.erl
@@ -5,5 +5,5 @@
 init(Req0, Opts) ->
 	Req = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/plain">>
-	}, <<"Hello world!">>, Req0),
+	}, <<"Hello from docker!">>, Req0),
 	{ok, Req, Opts}.
